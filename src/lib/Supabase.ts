5b9e50b011fbdcc56e7058c3b3030d73ce1b8f4c
daniel-amekpoagbe-yawson <amekpoagbedaniel@gmail.com>
2026-01-04 00:58:11 +0000

    corrected some styling issues

--- src/lib/Supabase.ts
+++ src/lib/Supabase.ts
@@ -18,7 +18,7 @@ export const supabase = createClient(
 
       // Auto-refresh session
       autoRefreshToken: true,
-      persistSession: true,
+      persistSession: false,
       detectSessionInUrl: true,
     },
   }
